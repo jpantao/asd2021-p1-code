9cedd1def7b99c6764a38b1d38ab7b24f3bf20e0
João <jp.antao@campus.fct.unl.pt>
2020-10-26 22:33:34 +0000

    hyparview base codead apted from the fullmembership protocol

--- src/main/java/protocols/membership/hyparview/HyParView.java
+++ src/main/java/protocols/membership/hyparview/HyParView.java
@@ -1,6 +1,90 @@
 package protocols.membership.hyparview;
 
-public class HyParView {
+import babel.core.GenericProtocol;
+import babel.exceptions.HandlerRegistrationException;
+import channel.tcp.TCPChannel;
+import channel.tcp.events.ChannelMetrics;
+import network.data.Host;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
+import java.util.Properties;
+
+public class HyParView extends GenericProtocol {
+
+    private static final Logger logger = LogManager.getLogger(HyParView.class);
+
+    //Protocol information, to register in babel
+    public static final short PROTOCOL_ID = 101;
+    public static final String PROTOCOL_NAME = "HyParView";
+
+    private final Host self; //My own address/port
+
+    private final int channelId; //Id of the created channel
+
+    public HyParView(Properties props, Host self) throws IOException {
+        super(PROTOCOL_NAME, PROTOCOL_ID);
+
+        this.self = self;
+
+        //Load properties
+        String cMetricsInterval = props.getProperty("channel_metrics_interval", "10000"); //10 seconds
+
+        //Create a properties object to setup channel-specific properties. See the channel description for more details.
+        Properties channelProps = new Properties();
+        channelProps.setProperty(TCPChannel.ADDRESS_KEY, props.getProperty("address")); //The address to bind to
+        channelProps.setProperty(TCPChannel.PORT_KEY, props.getProperty("port")); //The port to bind to
+        channelProps.setProperty(TCPChannel.METRICS_INTERVAL_KEY, cMetricsInterval); //The interval to receive channel metrics
+        channelProps.setProperty(TCPChannel.HEARTBEAT_INTERVAL_KEY, "1000"); //Heartbeats interval for established connections
+        channelProps.setProperty(TCPChannel.HEARTBEAT_TOLERANCE_KEY, "3000"); //Time passed without heartbeats until closing a connection
+        channelProps.setProperty(TCPChannel.CONNECT_TIMEOUT_KEY, "1000"); //TCP connect timeout
+        channelId = createChannel(TCPChannel.NAME, channelProps); //Create the channel with the given properties
+
+        /*---------------------- Register Message Serializers ---------------------- */
+
+        /*---------------------- Register Message Handlers ------------------------- */
+
+        /*---------------------- Register Timer Handlers --------------------------- */
+
+        /*---------------------- Register Channel Events --------------------------- */
+    }
+
+    @Override
+    public void init(Properties properties) throws HandlerRegistrationException, IOException {
 
     }
+
+    /*--------------------------------- Messages ----------------------------------- */
+
+    /*--------------------------------- Timers ------------------------------------- */
+
+    /* --------------------------------- TCPChannel Events ------------------------- */
+
+    /* --------------------------------- Metrics ----------------------------------- */
+
+    //If we passed a value > 0 in the METRICS_INTERVAL_KEY property of the channel, this event will be triggered
+    //periodically by the channel. This is NOT a protocol timer, but a channel event.
+    //Again, we are just showing some of the information you can get from the channel, and use how you see fit.
+    //"getInConnections" and "getOutConnections" returns the currently established connection to/from me.
+    //"getOldInConnections" and "getOldOutConnections" returns connections that have already been closed.
+    private void uponChannelMetrics(ChannelMetrics event, int channelId) {
+        StringBuilder sb = new StringBuilder("Channel Metrics:\n");
+        sb.append("In channels:\n");
+        event.getInConnections().forEach(c -> sb.append(String.format("\t%s: msgOut=%s (%s) msgIn=%s (%s)\n",
+                c.getPeer(), c.getSentAppMessages(), c.getSentAppBytes(), c.getReceivedAppMessages(),
+                c.getReceivedAppBytes())));
+        event.getOldInConnections().forEach(c -> sb.append(String.format("\t%s: msgOut=%s (%s) msgIn=%s (%s) (old)\n",
+                c.getPeer(), c.getSentAppMessages(), c.getSentAppBytes(), c.getReceivedAppMessages(),
+                c.getReceivedAppBytes())));
+        sb.append("Out channels:\n");
+        event.getOutConnections().forEach(c -> sb.append(String.format("\t%s: msgOut=%s (%s) msgIn=%s (%s)\n",
+                c.getPeer(), c.getSentAppMessages(), c.getSentAppBytes(), c.getReceivedAppMessages(),
+                c.getReceivedAppBytes())));
+        event.getOldOutConnections().forEach(c -> sb.append(String.format("\t%s: msgOut=%s (%s) msgIn=%s (%s) (old)\n",
+                c.getPeer(), c.getSentAppMessages(), c.getSentAppBytes(), c.getReceivedAppMessages(),
+                c.getReceivedAppBytes())));
+        sb.setLength(sb.length() - 1);
+        logger.info(sb);
+    }
+}
