3fb938fa43aa80cd2fb0da8e0ad6a20a9e5a7555
João <jp.antao@campus.fct.unl.pt>
2020-11-15 03:59:38 +0000

    chown fix

--- docker/start.sh
+++ docker/start.sh
@@ -6,5 +6,5 @@ shift 3
 
 logsfilename="logs/$experiment/node$idx"
 java -DlogFilename="$logsfilename" -cp asdProj.jar Main -conf "config$experiment.properties" "$@" &>/proc/1/fd/1
-chown $user logs/node$idx.log
+chown $user logs/$experiment/node$idx.log
 
