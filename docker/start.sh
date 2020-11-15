688ed9414ee13edd6105f4755a874a97eee26916
João <jp.antao@campus.fct.unl.pt>
2020-11-15 03:54:13 +0000

    start fix

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 experiment=$1
 idx=$2
+user=$3
+shift 3
+
 logsfilename="logs/$experiment/node$idx"
-shift 2
 java -DlogFilename="$logsfilename" -cp asdProj.jar Main -conf "config$experiment.properties" "$@" &>/proc/1/fd/1
