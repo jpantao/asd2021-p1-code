0dca8d0aa5444d09c4c9fd4b5d02e734025faf7d
João <jp.antao@campus.fct.unl.pt>
2020-11-16 20:14:15 +0000

    run1 csv

--- evaluation/evaluate.sh
+++ evaluation/evaluate.sh
@@ -4,32 +4,30 @@ if [[ -z $1 ]]; then
   echo 'Usage: ./evaluate.sh <experiment>'
   exit 1
 fi
-dir="csvs/$1"
-experimentPath="../logs/$1/*.log"
-if [[ ! -e "csvs" ]]; then
-  #mkdir "csvs"
-fi
-if [[ ! -e "csvs/$1" ]]; then
-  #mkdir "csvs/$1"
-fi
-if [[ ! -e $dir ]]; then
-  mkdir $dir
-fi
+experiment=$1
+run=$2
+experimentPath="../logs/run$run/$experiment/*.log"
+channelMetrics="csvs/channelMetrics"
+latencies="csvs/latencies"
+reliabilities="csvs/reliabilities"
+
+mkdir -p $channelMetrics
+mkdir -p $latencies
+mkdir -p $reliabilities
+
 nNodes=$(ls $experimentPath | wc -l)
 echo "Generating reliability.csv"
 # shellcheck disable=SC2126
-echo "Total:,$(grep "BroadcastApp" $experimentPath | grep "Sending" | wc -l)" >"$dir/reliability.csv"
+echo "Total:,$(grep "BroadcastApp" $experimentPath | grep "Sending" | wc -l)" >>"$reliabilities/'$experiment'reliability.csv"
 # shellcheck disable=SC2004
 for i in $(seq 00 $(($nNodes - 1))); do
   # shellcheck disable=SC2126
-  echo "$i,$(grep "BroadcastApp" "../logs/$1/node$i.log" | grep "Received" | wc -l)" >>"$dir/reliability.csv"
+  echo "$i,$(grep "BroadcastApp" "../logs/run$run/$experiment/node$i.log" | grep "Received" | wc -l)" >>"$reliabilities/'$experiment'reliability.csv"
 done
 
 function generateChannelMetrics() {
   echo "Generating ChannelMetrics.csv"
-  filename="$dir/ChannelMetrics.csv"
-  echo "InConnections,,,, OutConnections,,," >$filename
-  echo "MsgOut,BytesOut,MsgIn,BytesIn,MsgOut,BytesOut,MsgIn,BytesIn" >>$filename
+  filename="$channelMetrics/'$experiment'ChannelMetrics.csv"
   declare -a metrics
   i=0
   l=0
@@ -39,9 +37,7 @@ function generateChannelMetrics() {
   # shellcheck disable=SC2207
   aux=($(cat $experimentPath | grep -o "ChannelMetrics.*" | cut -f2- -d:))
   for ((k = 0; k < ${#aux[@]}; k += 1)); do
-    echo "${aux[$k]}"
     for j in $(echo "${aux[$k]}" | tr "=" "\n" | grep "[0-9]" | tr -d "[a-zA-Z]"); do
-      echo "j: $j"
       l=$((l + 1))
       metrics[l]=$j
     done
@@ -63,7 +59,6 @@ function convertToMillis() {
 }
 function generateLatency() {
   echo "Generating latency.csv"
-  echo "SentTime, ReceivedTime" >"$dir/latency.csv"
   # shellcheck disable=SC2207
   sentMids=($(cat $experimentPath | grep "BroadcastApp" | grep "Sending" | sort | tr " " "\n" | grep '.\{36\}'))
   # shellcheck disable=SC2207
@@ -72,16 +67,16 @@ function generateLatency() {
   receivedMids=($(cat $experimentPath | grep "BroadcastApp" | grep "Received" | sort | tr " " "\n" | grep '.\{36\}'))
   # shellcheck disable=SC2207
   receivedLatency=($(cat $experimentPath | grep "BroadcastApp" | grep "Received" | sort | grep -o -P 'I.{0,13}' | grep : | tr -d "I["))
-  for ((i = 0; i < ${#sentMids[@]}; i += nNodes/2)); do
+  for ((i = 0; i < ${#sentMids[@]}; i += 5)); do
     l=0
-    latencies="$(convertToMillis ${receivedLatency[$i]})"
+    latencies_="$(convertToMillis ${receivedLatency[$i]})"
     for ((k = 0; k < ${#receivedMids[@]} && l < nNodes; k++)); do
       if [[ "${receivedMids[$k]}" == "${sentMids[$i]}" ]]; then
         l=$((l + 1))
-        latencies="${latencies},$(convertToMillis ${receivedLatency[$k]})"
+        latencies_="${latencies_},$(convertToMillis ${receivedLatency[$k]})"
       fi
     done
-    echo $latencies >>"$dir/latency.csv"
+    echo $latencies_ >>"$latencies/'$experiment'latency.csv"
   done
 }
 
--- evaluation/generate_csvs.sh
+++ evaluation/generate_csvs.sh
@@ -2,6 +2,6 @@
 
 for experiment in $(seq 2 2 24); do
 	echo "Generating plots for: $experiment"
-	./evaluate.sh $experiment
+	./evaluate.sh $experiment $1
 	wait
 done
